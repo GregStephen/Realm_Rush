5f810f55d63954845922a64677bdedb14288c6cf
Greg Stephen <GregAStephen@gmail.com>
2020-06-28 21:23:53 -0500

    added tower range and ability to have more than one enemy on path

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int hits = 10;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hits = hits - 1;
+    }
+
+    private void KillEnemy()
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] ParticleSystem turret;
+    [SerializeField] float attackRange = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            ProcessFiring();
+        }
+        else
+        {
+            SetGunsActive(false);
+        }
+
+    }
+
+    private void ProcessFiring()
+    {
+        var distanceToEnemy = Vector3.Distance(targetEnemy.position, gameObject.transform.position);
+        if (distanceToEnemy <= attackRange)
+        {
+            SetGunsActive(true);
+        }
+        else
+        {
+            SetGunsActive(false);
+        }
+    }
+    private void SetGunsActive(bool isActive)
+    {
+        var emissionModule = turret.emission;
+        emissionModule.enabled = isActive;
+        
+    }
+}
